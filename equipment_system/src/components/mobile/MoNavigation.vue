9f8f73c88ce1dd6000444f9c86442169bf8b5e10
Elaine <1807332757@qq.com>
2020-12-21 18:46:32 +0800

    style: 首页手机端去掉导航栏

--- equipment_system/src/components/mobile/MoNavigation.vue
+++ equipment_system/src/components/mobile/MoNavigation.vue
@@ -1,56 +1,56 @@
 <template>
   <div>
     <a-layout id="components-layout-demo-responsive">
-      <a-layout-sider
-        breakpoint="lg"
-        collapsed-width="0"
-        @collapse="onCollapse"
-        @breakpoint="onBreakpoint"
-      >
-        <a-menu
-          :default-selected-keys="['1']"
-          :default-open-keys="['sub1','sub2']"
-          mode="inline"
-          theme="dark"
-          :inline-collapsed="collapsed"
-          class="nav"
-        >
-          <a-menu-item key="1" class="home" @click="navPage('/index')">
-            <a-icon type="home" />
-            <span>首页</span>
-          </a-menu-item>
-          <a-sub-menu key="sub1">
-            <span slot="title"><a-icon type="database"/><span>基本数据</span></span>
-            <a-menu-item key="2" @click="navPage('/devInfo')">
-              设备信息
-            </a-menu-item>
-            <a-menu-item key="3" @click="navPage('/equitmentPartInfo')">
-              设备零件信息
-            </a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <span slot="title"><a-icon type="setting" /><span>查询管理</span></span>
-            <a-menu-item key="4" @click="navPage('/maintainData')">
-              维护数据
-            </a-menu-item>
-            <a-menu-item key="5" @click="navPage('/repairData')">
-              维修数据
-            </a-menu-item>
-            <a-menu-item key="6" @click="navPage('/changeData')">
-              更换数据
-            </a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <span slot="title"><a-icon type="user" /><span>账户信息</span></span>
-            <a-menu-item key="7" @click="navPage('/personalInfo')">
-              个人信息
-            </a-menu-item>
-            <a-menu-item key="8" @click="navPage('/userManagement')">
-              用户管理
-            </a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
+<!--      <a-layout-sider-->
+<!--        breakpoint="lg"-->
+<!--        collapsed-width="0"-->
+<!--        @collapse="onCollapse"-->
+<!--        @breakpoint="onBreakpoint"-->
+<!--      >-->
+<!--        <a-menu-->
+<!--          :default-selected-keys="['1']"-->
+<!--          :default-open-keys="['sub1','sub2']"-->
+<!--          mode="inline"-->
+<!--          theme="dark"-->
+<!--          :inline-collapsed="collapsed"-->
+<!--          class="nav"-->
+<!--        >-->
+<!--          <a-menu-item key="1" class="home" @click="navPage('/index')">-->
+<!--            <a-icon type="home" />-->
+<!--            <span>首页</span>-->
+<!--          </a-menu-item>-->
+<!--          <a-sub-menu key="sub1">-->
+<!--            <span slot="title"><a-icon type="database"/><span>基本数据</span></span>-->
+<!--            <a-menu-item key="2" @click="navPage('/devInfo')">-->
+<!--              设备信息-->
+<!--            </a-menu-item>-->
+<!--            <a-menu-item key="3" @click="navPage('/equitmentPartInfo')">-->
+<!--              设备零件信息-->
+<!--            </a-menu-item>-->
+<!--          </a-sub-menu>-->
+<!--          <a-sub-menu key="sub2">-->
+<!--            <span slot="title"><a-icon type="setting" /><span>查询管理</span></span>-->
+<!--            <a-menu-item key="4" @click="navPage('/maintainData')">-->
+<!--              维护数据-->
+<!--            </a-menu-item>-->
+<!--            <a-menu-item key="5" @click="navPage('/repairData')">-->
+<!--              维修数据-->
+<!--            </a-menu-item>-->
+<!--            <a-menu-item key="6" @click="navPage('/changeData')">-->
+<!--              更换数据-->
+<!--            </a-menu-item>-->
+<!--          </a-sub-menu>-->
+<!--          <a-sub-menu key="sub3">-->
+<!--            <span slot="title"><a-icon type="user" /><span>账户信息</span></span>-->
+<!--            <a-menu-item key="7" @click="navPage('/personalInfo')">-->
+<!--              个人信息-->
+<!--            </a-menu-item>-->
+<!--            <a-menu-item key="8" @click="navPage('/userManagement')">-->
+<!--              用户管理-->
+<!--            </a-menu-item>-->
+<!--          </a-sub-menu>-->
+<!--        </a-menu>-->
+<!--      </a-layout-sider>-->
       <a-layout>
         <a-layout-header class="header">
           <div class="nav-title flex-row">
