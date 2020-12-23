23d79bf8547e21f08d5fbf70a8ae3d5505e2f141
Elaine <1807332757@qq.com>
2020-12-23 11:08:37 +0800

    feat: 封装axios中get和post请求

---
+++ equipment_system/src/api/index.js
@@ -0,0 +1,7 @@
+import {get,post} from './http';
+
+//判断用户是否登录成功
+export const getLoginStatus = (params) => post(`cosumer/login/status`,params);
+
+//获取商品列表
+export const getCommodityList =() => get(`commodity/getList`);
